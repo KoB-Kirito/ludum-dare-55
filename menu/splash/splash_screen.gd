bd33f65ead707f7ddbe8110f38edb0e646419408
Kirito <kilito@pm.me>
2024-04-13 10:55:18 +0200

    Add splash screen

---
+++ menu/splash/splash_screen.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+@export var next_scene: PackedScene
+
+@export_group("Fade In")
+@export var fade_in_duration: float = 1.0
+@export var fade_in_transition: Tween.TransitionType
+@export var fade_in_ease: Tween.EaseType
+
+@export_group("Stay")
+@export var stay_duration: float = 3.0
+
+@export_group("Fade Out")
+@export var fade_out_duration: float = 1.0
+@export var fade_out_transition: Tween.TransitionType
+@export var fade_out_ease: Tween.EaseType
+
+
+func _ready() -> void:
+	var tween := create_tween()
+	tween.tween_interval(1.0)
+	# godot
+	tween.tween_property(%Fade, "modulate", Color.TRANSPARENT, fade_in_duration).set_trans(fade_in_transition).set_ease(fade_in_ease)
+	tween.tween_interval(stay_duration)
+	tween.tween_property(%Fade, "modulate", Color.WHITE, fade_out_duration).set_trans(fade_out_transition).set_ease(fade_out_ease)
+	
+	#TODO: Team splash, Best played with controller?, ...
+	
+	tween.tween_callback(func(): get_tree().change_scene_to_packed(next_scene))
