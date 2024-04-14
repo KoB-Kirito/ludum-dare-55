5474fb29becee8274b8ce591fea88bcf65a7a26b
HairaNight <157826367+HairaNight@users.noreply.github.com>
2024-04-14 18:24:25 +0200

    added test enemy
    health, speed and hit strength adjustable in editor

--- game/enemies/enemy.gd
+++ game/enemies/enemy.gd
@@ -2,17 +2,53 @@ class_name Enemy
 extends CharacterBody3D
 
 @export var health: int = 6
-@export var speed = 3
+@export var speed = 2
+@export var hit_strength = 10
+
+var start_pos
+var start_rot
+var is_attacking: bool = false
+var player
+
+func _ready():
+	start_pos = self.position
+	start_rot = self.rotation
 
 func _physics_process(_delta):
-	move_and_slide()
-
-func _on_detection_zone_body_entered(body) -> void:
-	if body is Player:
-		var player_pos = body.get_global_position()
-		var enemy_pos = self.get_global_position()
+	#enemy movement
+	var enemy_pos = self.get_global_position()
+	if is_attacking == true:
+		var player_pos = player.get_global_position()
 		#look at player
 		look_at_from_position(enemy_pos,player_pos,Vector3.UP)
 		#move towards player
 		velocity = Vector3.FORWARD * speed
 		velocity = velocity.rotated(Vector3.UP, rotation.y)
+	if is_attacking == false and enemy_pos != start_pos:
+		#back to guard post
+		look_at_from_position(enemy_pos, start_pos, Vector3.UP)
+		velocity = Vector3.FORWARD * speed
+		velocity = velocity.rotated(Vector3.UP, rotation.y)
+		if enemy_pos.distance_to(start_pos) < 0.2:
+			velocity = Vector3()
+			position = start_pos
+			rotation = start_rot
+	move_and_slide()
+
+func _on_detection_zone_body_entered(body) -> void:
+	if body is Player:
+		player = body
+		is_attacking = true
+
+func _on_detection_zone_body_exited(body):
+	if body is Player:
+		is_attacking = false
+
+func damage(amount):
+	health -= amount
+	if health <= 0:
+		queue_free()
+
+func _on_hit_zone_body_entered(body):
+	if body is Player:
+		body.damage(hit_strength)
