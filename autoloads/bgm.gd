44308f76ed6140ef02187848d173eac98d1d8bb6
Kirito <kilito@pm.me>
2024-04-14 13:19:36 +0200

    Add fade and ability to change song to bgm player

--- autoloads/bgm.gd
+++ autoloads/bgm.gd
@@ -1,5 +1,32 @@
 extends AudioStreamPlayer
 
 
+const SILENCE = -60.0
+
+## Used if stream is set and autoplay is enabled
+@export var initial_fade_in_duration: float = 1.0
+
+
 func _ready() -> void:
-	play()
+	if autoplay and stream != null:
+		stop()
+		fade_to(stream, volume_db, initial_fade_in_duration * 2)
+
+
+## Fades out old song, fades in new song
+func fade_to(new_stream: AudioStream, volume: float, duration: float) -> void:
+	var tween = create_tween()
+	
+	if playing:
+		if duration > 0:
+			tween.tween_property(self, "volume_db", SILENCE, duration / 2)
+		tween.tween_callback(func(): stop())
+	
+	tween.tween_callback(func(): volume_db = SILENCE)
+	tween.tween_callback(func(): stream = new_stream)
+	tween.tween_callback(func(): play())
+	
+	if duration > 0:
+		tween.tween_property(self, "volume_db", volume, duration / 2)
+	else:
+		tween.tween_callback(func(): volume_db = volume)
