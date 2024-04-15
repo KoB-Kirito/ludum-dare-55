e16559480b9784f2b9de31c173e76c3c5b5249b3
Kirito <kilito@pm.me>
2024-04-15 13:17:00 +0200

    Add live switches, add timers to switches, switches work in editor now

--- game/interactables/switch.gd
+++ game/interactables/switch.gd
@@ -1,19 +1,42 @@
+@tool
 extends Interactable
 
 
-## Will start in that position
-@export_enum("Up", "Down") var state: int = 0
-## Can't be triggered again
-@export var only_once: bool = false
+## Open and close in editor
+@export var press: bool:
+	set(value):
+		if Engine.is_editor_hint():
+			interact()
+	get:
+		return false
+
+@export_subgroup("Settings")
+## Can't be triggered again. If timer is set can only be triggered when timer is not running.
+@export var only_once: bool = false:
+	set(value):
+		only_once = value
+		if not only_once:
+			triggered = false
+			reset_timer = -1
+
+## Time until it triggers again in seconds. -1 = Will stay
+@export_range(-1, 30, 1) var reset_timer: float = -1:
+	set(value):
+		reset_timer = value
+		if value > 0:
+			only_once = true
+
+@export_group("Targets")
 ## Will trigger all targets at once
 @export var targets: Array[Triggerable]
 
+var is_pressed: bool
 var triggered: bool
+var reset: bool
 
 
 func _ready() -> void:
-	if state:
-		%AnimationPlayer.play("down")
+	triggered = false
 
 
 func interact() -> void:
@@ -22,14 +45,31 @@ func interact() -> void:
 			return
 		triggered = true
 	
+	if reset:
+		reset = false
+		triggered = false
+	else:
+		if reset_timer > 0:
+			%Timer.start(reset_timer)
+			%snd_timer.play()
+			triggered = true
+	
 	%snd_press.play()
-	if state:
+	
+	if is_pressed:
 		%AnimationPlayer.play("up")
-		state = 0
+		is_pressed = false
 	else:
 		%AnimationPlayer.play("down")
-		state = 1
+		is_pressed = true
 	
 	for target: Triggerable in targets:
 		if is_instance_valid(target):
 			target.trigger()
+
+
+func _on_timer_timeout() -> void:
+	%snd_timer.stop()
+	triggered = false
+	reset = true
+	interact()
