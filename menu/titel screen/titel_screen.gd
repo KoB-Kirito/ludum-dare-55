e6974fb0dfc6d5ace0906028f13c551f9eb30183
HairaNight <157826367+HairaNight@users.noreply.github.com>
2024-04-15 22:11:15 +0200

    Titel screen

--- menu/titel screen/titel_screen.gd
+++ menu/titel screen/titel_screen.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/levels/Credits.tscn")
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/levels/Intro.tscn")
